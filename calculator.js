f733157863af81ac664e4545d7f5d76c1aab776a
barryadrianallen <barryadrianallen@gmail.com>
2022-09-14 21:01:16 +0100

    implement key press functionality

--- calculator.js
+++ calculator.js
@@ -2,7 +2,16 @@ const displayScreen = document.querySelector("#display");
 const buttonOne = document.querySelector("#one");
 const buttonTwo = document.querySelector("#two");
 const buttonThree = document.querySelector("#three");
+const buttonFour = document.querySelector("#four");
+const buttonFive = document.querySelector("#five");
+const buttonSix = document.querySelector("#six");
+const buttonSeven = document.querySelector("#seven");
+const buttonEight = document.querySelector("#eight");
+const buttonNine = document.querySelector("#nine");
 const plusButton = document.querySelector("#plus");
+const minusButton = document.querySelector("#minus");
+const multiplyButton = document.querySelector("#multiply");
+const divideButton = document.querySelector("#divide");
 const equalsButton = document.querySelector("#equals");
 
 let app = {
@@ -89,91 +98,94 @@ document.addEventListener('keypress', (event) => {
             break;
         case "1":
             buttonOne.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() => {
                 buttonOne.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "2":
             buttonTwo.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() => {
                 buttonTwo.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "3":
             buttonThree.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() => {
                 buttonThree.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "4":
             buttonFour.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() =>{
                 buttonFour.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break; 
         case "5":
             buttonFive.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() => {
                 buttonFive.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "6":
             buttonSix.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() => {
                 buttonSix.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "7":
             buttonSeven.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() =>{
                 buttonSeven.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "8":
             buttonEight.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() =>{
                 buttonEight.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "9":
             buttonNine.classList.add("active");
-            setTimeout(function() {
+            setTimeout(() =>{
                 buttonNine.classList.remove("active");
             }, 100);
-            updateUserValue(event.key);
+            handleNumberClick(event.key);
             break;
         case "+":
         plusButton.classList.add("activeOperator");
-        //setTimeout(), this time with arrow functions
         setTimeout(() => {
             plusButton.classList.remove("activeOperator");
         }, 100);
+        handleOperationClick(event.key);
             break;
         case "-":
             minusButton.classList.add("activeOperator");
             setTimeout(() => {
                 minusButton.classList.remove("activeOperator");
             }, 100);
+            handleOperationClick(event.key);
             break;
         case "*":
             multiplyButton.classList.add("activeOperator");
             setTimeout(() => {
                 multiplyButton.classList.remove("activeOperator");
             }, 100);
+            handleOperationClick(event.key);
             break;
         case "/":
             divideButton.classList.add("activeOperator");
             setTimeout(() => {
                 divideButton.classList.remove("activeOperator");
             }, 100);
+            handleOperationClick(event.key);
             break;
         case "=":
             equalsButton.classList.add("activeOperator");
